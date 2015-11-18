323c44407dbb6948b9a25c9f69e3edacaa77016a
depekur <depekur@gmail.com>
2015-11-18 13:38:43 +0300

    ad all angular.js files via wordpress

--- wp-content/themes/angular_ecommerce/functions/add_scripts.php
+++ wp-content/themes/angular_ecommerce/functions/add_scripts.php
@@ -1,12 +1,49 @@
 <?php
 
-add_action( 'wp_enqueue_scripts', 'add_my_super_app' );
+add_action( 'wp_enqueue_scripts', 'add_scripts' );
 
-function add_my_super_app() {
+function add_scripts() {
 
-   wp_enqueue_script('brick_app', 
-      get_template_directory_uri().'/js/app.js', 
-      array('jquery'), '1.0', true );
+   wp_enqueue_script('angular', 
+					      get_template_directory_uri() . '/bower_components/angular/angular.js', 
+					      false,
+					      '1.0',
+					      false );
+   wp_enqueue_script('angular-animate', 
+					      get_template_directory_uri() . '/bower_components/angular-animate/angular-animate.js', 
+					      array( 'angular' ),
+					      '1.0',
+					      false );
+   wp_enqueue_script('angular-route', 
+					      get_template_directory_uri() . '/bower_components/angular-route/angular-route.js', 
+					      array( 'angular' ),
+					      '1.0',
+					      false );
+   wp_enqueue_script('angular-resource', 
+					      get_template_directory_uri() . '/bower_components/angular-resource/angular-resource.js', 
+					      array( 'angular' ),
+					      '1.0',
+					      false );
+   wp_enqueue_script('app', 
+					      get_template_directory_uri() . '/js/app.js', 
+					      array( 'angular' ),
+					      '1.0',
+					      false );
+   wp_enqueue_script('controller', 
+					      get_template_directory_uri() . '/js/controller.js', 
+					      array( 'angular' ),
+					      '1.0',
+					      false );
+   wp_enqueue_script('services', 
+					      get_template_directory_uri() . '/js/services.js', 
+					      array( 'angular' ),
+					      '1.0',
+					      false );
+   wp_enqueue_script('filters', 
+					      get_template_directory_uri() . '/js/filters.js', 
+					      array( 'angular' ),
+					      '1.0',
+					      false );
    
    $protocol = isset( $_SERVER["HTTPS"] ) ? 'https://' : 'http://';
 
@@ -14,5 +51,5 @@ function add_my_super_app() {
       'ajaxurl' => admin_url('admin-ajax.php', '$protocol')
    );
 
-   wp_localize_script( 'brick_app', 'brick_app', $params );
+   wp_localize_script( 'angular', 'cookbook', $params );
 } 
--- wp-content/themes/angular_ecommerce/header.php
+++ wp-content/themes/angular_ecommerce/header.php
@@ -6,15 +6,6 @@
 
 	<link rel="stylesheet" href="<?= get_template_directory_uri() . '/style.css'; ?>">
 
-	<script src="<?= get_template_directory_uri() . '/bower_components/angular/angular.js'; ?>"></script>
-	<script src="<?= get_template_directory_uri() . '/bower_components/angular-animate/angular-animate.js'; ?>"></script>
-	<script src="<?= get_template_directory_uri() . '/bower_components/angular-route/angular-route.js'; ?>"></script>
-	<script src="<?= get_template_directory_uri() . '/bower_components/angular-resource/angular-resource.js'; ?>"></script>
-	<script src="<?= get_template_directory_uri() . '/js/app.js'; ?>"></script>
-	<script src="<?= get_template_directory_uri() . '/js/controller.js'; ?>"></script>
-	<script src="<?= get_template_directory_uri() . '/js/services.js'; ?>"></script>
-	<script src="<?= get_template_directory_uri() . '/js/controller.js'; ?>"></script>
-	<script src="<?= get_template_directory_uri() . '/js/filters.js'; ?>"></script>
 
 
 	<?php wp_head(); ?>
